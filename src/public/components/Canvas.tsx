e1afad5c85103b731be4d1040ec32434ec9597c1
Thomas <Thomas@DevBook>
2017-11-28 20:00:04 +0100

    added drawing handler

--- src/public/components/Canvas.tsx
+++ src/public/components/Canvas.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Line } from "../models/Line";
 import { CanvasTransform } from "../services/CanvasTransform";
+import { DrawingHandler } from "../services/DrawingHandler";
 import { tapEvents } from "../services/TapEvents";
 
 export enum DrawMode {
@@ -22,6 +23,7 @@ export interface CanvasProps {
 }
 
 export default class Canvas extends React.Component<CanvasProps> {
+    private drawingHandler: DrawingHandler;
     private linesGroupedByColorAndWidth: Line[][];
     private canvas: HTMLCanvasElement | null;
     private canvasTransform: CanvasTransform;
@@ -34,6 +36,7 @@ export default class Canvas extends React.Component<CanvasProps> {
     constructor() {
         super();
         this.canvasTransform = new CanvasTransform();
+        this.drawingHandler = new DrawingHandler();
 
         this.tapDown = this.tapDown.bind(this);
         this.tapUp = this.tapUp.bind(this);
@@ -102,7 +105,10 @@ export default class Canvas extends React.Component<CanvasProps> {
                     lineWidth: canvasContext.lineWidth,
                     segments: []
                 };
-                this.drawLine(canvasContext, pt.x, pt.y, pt.x + 0.1, pt.y + 0.1);
+                this.drawingHandler.drawSegment(
+                    canvasContext,
+                    { x: pt.x, y: pt.y },
+                    { x: pt.x + 0.1, y: pt.y + 0.1 });
 
                 break;
             case PenMode.Translate:
@@ -128,7 +134,10 @@ export default class Canvas extends React.Component<CanvasProps> {
 
         switch (this.currentPenMode) {
             case PenMode.Draw:
-                this.drawLine(canvasContext, this.tapDownPoint.x, this.tapDownPoint.y, pt.x, pt.y);
+                this.drawingHandler.drawSegment(
+                    canvasContext,
+                    this.tapDownPoint,
+                    pt);
 
                 const segment = {
                     end: { x: pt.x, y: pt.y },
@@ -239,128 +248,14 @@ export default class Canvas extends React.Component<CanvasProps> {
         // faster, bot wrong detail (lines are not in the right order)
         // this.drawLinesSortedAndGrouped(context, { left, top, right, bottom });
         // } else {
-        this.drawLines(context, { left, top, right, bottom });
+        this.drawingHandler.drawLines(
+            context,
+            this.props.lines,
+            { left, top, right, bottom });
         // }
 
         context.lineWidth = this.props.lineWidth;
         context.strokeStyle = this.props.color;
         context.globalCompositeOperation = this.props.drawMode === DrawMode.Above ? "source-over" : "destination-over";
     }
-
-    private drawLine(canvasContext: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
-        canvasContext.beginPath();
-        canvasContext.moveTo(x1, y1);
-        canvasContext.lineTo(x2, y2);
-        canvasContext.stroke();
-        canvasContext.closePath();
-    }
-
-    private drawLines(
-        canvasContext: CanvasRenderingContext2D,
-        boundary: {
-            left: number,
-            top: number,
-            right: number,
-            bottom: number
-        }) {
-
-        const { top, left, right, bottom } = boundary;
-
-        this.props.lines.forEach((line) => {
-            canvasContext.beginPath();
-            canvasContext.strokeStyle = line.color;
-            canvasContext.lineWidth = line.lineWidth;
-            canvasContext.globalCompositeOperation = line.globalCompositeOperation;
-
-            line.segments.forEach((segment) => {
-                if (
-                    (segment.start.x > left
-                        && segment.start.x < right
-                        && segment.start.y > top
-                        && segment.start.y < bottom)
-                    ||
-                    (segment.end.x > left
-                        && segment.end.x < right
-                        && segment.end.y > top
-                        && segment.end.y < bottom)) {
-                    canvasContext.moveTo(segment.start.x, segment.start.y);
-                    canvasContext.lineTo(segment.end.x, segment.end.y);
-                }
-            });
-
-            canvasContext.stroke();
-            canvasContext.closePath();
-        });
-    }
-
-    // private refreshLinesGroupedByColorAndWidth() {
-    //     this.linesGroupedByColorAndWidth =
-    //         groupBy(
-    //             this.props.lines.sort((l1: Line, l2: Line) => l1.color > l2.color ? 1 : -1),
-    //             (line) => `${line.color}|${line.lineWidth}|${line.globalCompositeOperation}`);
-    // }
-
-    // private drawLinesSortedAndGrouped(
-    //     canvasContext: CanvasRenderingContext2D,
-    //     boundary: {
-    //         left: number,
-    //         top: number,
-    //         right: number,
-    //         bottom: number
-    //     }) {
-
-    //     const { top, left, right, bottom } = boundary;
-
-    //     for (const key in this.linesGroupedByColorAndWidth) {
-    //         if (!this.linesGroupedByColorAndWidth.hasOwnProperty(key)) {
-    //             continue;
-    //         }
-
-    //         const lines = this.linesGroupedByColorAndWidth[key];
-
-    //         canvasContext.beginPath();
-    //         const [color, lineWidth, globalCompositeOperation] = key.split("|");
-    //         canvasContext.strokeStyle = color;
-    //         canvasContext.lineWidth = +lineWidth;
-    //         canvasContext.globalCompositeOperation = globalCompositeOperation;
-
-    //         lines.forEach((line: Line) => {
-    //             line.segments.forEach((segment) => {
-    //                 if (
-    //                     (segment.start.x > left
-    //                         && segment.start.x < right
-    //                         && segment.start.y > top
-    //                         && segment.start.y < bottom)
-    //                     ||
-    //                     (segment.end.x > left
-    //                         && segment.end.x < right
-    //                         && segment.end.y > top
-    //                         && segment.end.y < bottom)) {
-    //                     canvasContext.moveTo(segment.start.x, segment.start.y);
-    //                     canvasContext.lineTo(segment.end.x, segment.end.y);
-    //                 }
-    //             });
-    //         });
-
-    //         canvasContext.stroke();
-    //         canvasContext.closePath();
-    //     }
-    // }
 }
-
-// from https://gist.github.com/bryandh/c0cd25df7ccfdf891bf782174535f4ca
-// function groupBy<T>(collection: T[], property: (item: T) => any): any {
-//     return collection.reduce(
-//         (prev: any, next: any) => {
-//             // Get the group name from the property function, this will be the index of the group in the array
-//             const groupName = property(next);
-//             // Set the group in the array to either its previous value (prev[groupName])
-//             // if that exists, otherwise fill it with an empty array
-//             prev[groupName] = prev[groupName] || [];
-//             // Add the next item to the group's array
-//             prev[groupName].push(next);
-//             return prev;
-//         },
-//         Object.create([])
-//     );
-// }
