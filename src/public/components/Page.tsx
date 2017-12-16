a5528fa01a41d255fda78128d417d89720275487
Thomas <Thomas@DevDisc>
2017-12-16 13:30:51 +0100

    made sketcharea a container component

--- src/public/components/Page.tsx
+++ src/public/components/Page.tsx
@@ -1,19 +1,10 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { NavLink, RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
 import { Dispatch } from "redux";
-import { addLine } from "../actions";
-import {
-    CompositeOperation,
-    FontSize,
-    InputMode,
-    Line,
-    Page as PageModel,
-    Point,
-    RootState
-} from "../models/RootState";
+import { Page as PageModel, Point, RootState } from "../models/RootState";
 import { Overview } from "./Overview";
-import { SketchArea } from "./SketchArea";
+import SketchArea from "./SketchArea";
 
 export interface PageRouteProps {
     id: string;
@@ -23,16 +14,7 @@ export interface PageOwnProps extends RouteComponentProps<PageRouteProps> {
 }
 
 export interface PageProps {
-    inputMode: InputMode;
-    fontSize: FontSize;
-    color: string;
-    lineWidth: number;
-    compositeOperation: CompositeOperation;
     page: PageModel;
-}
-
-interface PageDispatchProps {
-    onLineAdded: (line: Line) => void;
 }
 
 export interface PageState {
@@ -40,7 +22,7 @@ export interface PageState {
     sketchAreaCenter: Point;
 }
 
-class Page extends React.Component<PageProps & PageDispatchProps & PageOwnProps, PageState> {
+class Page extends React.Component<PageProps & PageOwnProps, PageState> {
     constructor(props: any) {
         super(props);
         this.onOverviewClick = this.onOverviewClick.bind(this);
@@ -50,17 +32,7 @@ class Page extends React.Component<PageProps & PageDispatchProps & PageOwnProps,
     public render() {
         const content = this.state.isOverview
             ? <Overview elements={this.props.page.elements} onClick={this.onOverviewClick} />
-            : (
-                <SketchArea
-                    inputMode={this.props.inputMode}
-                    fontSize={this.props.fontSize}
-                    center={this.state.sketchAreaCenter}
-                    color={this.props.color}
-                    compositeOperation={this.props.compositeOperation}
-                    lineWidth={this.props.lineWidth}
-                    elements={this.props.page.elements}
-                    onLineAdded={this.props.onLineAdded}
-                />);
+            : <SketchArea page={this.props.page} center={this.state.sketchAreaCenter} />;
 
         return (
             < React.Fragment >
@@ -75,66 +47,15 @@ class Page extends React.Component<PageProps & PageDispatchProps & PageOwnProps,
 }
 
 function mapStateToProps(state: RootState, ownProps: PageOwnProps): PageProps {
-    const { inputMode, fontSize, pen: { color, strokeWidth } } = state;
-    let page = state.pages.find((p) => p.id === ownProps.match.params.id);
+    const page = state.pages.find((p) => p.id === ownProps.match.params.id);
 
     if (!page) {
-        // TODO: redundant defintion of default value for page
-        page = { id: (state.pages.length + 1).toString(), elements: [] };
+        throw new Error(`Unknown Page with id ${ownProps.match.params.id}`);
     }
 
-    let colorHexCode: string;
-    let lineWidth: number;
-    let compositeOperation: CompositeOperation = "source-over";
-
-    switch (color) {
-        case "black":
-            colorHexCode = "#1d1d1d";
-            break;
-        case "grey":
-            colorHexCode = "#b9afb0";
-            compositeOperation = "destination-over";
-            break;
-        case "blue":
-            colorHexCode = "#4595d8";
-            compositeOperation = "destination-over";
-            break;
-        case "orange":
-            colorHexCode = "#f9a765";
-            compositeOperation = "destination-over";
-            break;
-        // TODO: redundant defintion of default value for color
-        default:
-            colorHexCode = "#1d1d1d";
-            break;
-    }
-
-    switch (strokeWidth) {
-        case "s":
-            lineWidth = 4;
-            break;
-        case "m":
-            lineWidth = 10;
-            break;
-        case "l":
-            lineWidth = 20;
-            break;
-        // TODO: redundant defintion of default value for lineWidth
-        default:
-            lineWidth = 4;
-            break;
-    }
-
-    return { color: colorHexCode, lineWidth, compositeOperation, page, inputMode, fontSize };
+    return { page };
 }
 
-function mapDispatchToProps(dispatch: Dispatch<RootState>, ownProps: PageOwnProps) {
-    return {
-        onLineAdded: (line: Line) => dispatch(addLine(ownProps.match.params.id, line))
-    };
-}
-
-export default connect<PageProps, PageDispatchProps, PageOwnProps, RootState>(
-    mapStateToProps,
-    mapDispatchToProps
+export default connect<PageProps, {}, PageOwnProps, RootState>(
+    mapStateToProps
 )(Page);
--- src/public/components/SketchArea.tsx
+++ src/public/components/SketchArea.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
+import { connect, Dispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { CompositeOperation, FontSize, InputMode, Line, PageElement, Point } from "../models/RootState";
+import { addLine } from "../actions";
+import { CompositeOperation, FontSize, InputMode, Line, Page, Point, RootState } from "../models/RootState";
 import Canvas from "./Canvas";
 import Menu from "./Menu";
 
 export interface SketchAreaProps {
     inputMode: InputMode;
     fontSize: FontSize;
-    center: Point;
     color: string;
     lineWidth: number;
     compositeOperation: CompositeOperation;
-    elements: PageElement[];
+}
+
+export interface SketchAreaOwnProps {
+    page: Page;
+    center: Point;
+}
+
+interface SketchAreaDispatchProps {
     onLineAdded: (line: Line) => void;
 }
 
-export const SketchArea: React.SFC<SketchAreaProps> = (props) => (
+const SketchArea: React.SFC<SketchAreaProps & SketchAreaDispatchProps & SketchAreaOwnProps> = (props) => (
     <React.Fragment>
         <Canvas
             inputMode={props.inputMode}
@@ -23,9 +31,68 @@ export const SketchArea: React.SFC<SketchAreaProps> = (props) => (
             color={props.color}
             compositeOperation={props.compositeOperation}
             lineWidth={props.lineWidth}
-            elements={props.elements}
+            elements={props.page.elements}
             onLineAdded={props.onLineAdded}
         />
         <Menu />
     </React.Fragment>
 );
+
+function mapStateToProps(state: RootState): SketchAreaProps {
+    const { inputMode, fontSize, pen: { color, strokeWidth } } = state;
+
+    let colorHexCode: string;
+    let lineWidth: number;
+    let compositeOperation: CompositeOperation = "source-over";
+
+    switch (color) {
+        case "black":
+            colorHexCode = "#1d1d1d";
+            break;
+        case "grey":
+            colorHexCode = "#b9afb0";
+            compositeOperation = "destination-over";
+            break;
+        case "blue":
+            colorHexCode = "#4595d8";
+            compositeOperation = "destination-over";
+            break;
+        case "orange":
+            colorHexCode = "#f9a765";
+            compositeOperation = "destination-over";
+            break;
+        // TODO: redundant defintion of default value for color
+        default:
+            colorHexCode = "#1d1d1d";
+            break;
+    }
+
+    switch (strokeWidth) {
+        case "s":
+            lineWidth = 4;
+            break;
+        case "m":
+            lineWidth = 10;
+            break;
+        case "l":
+            lineWidth = 20;
+            break;
+        // TODO: redundant defintion of default value for lineWidth
+        default:
+            lineWidth = 4;
+            break;
+    }
+
+    return { color: colorHexCode, lineWidth, compositeOperation, inputMode, fontSize };
+}
+
+function mapDispatchToProps(dispatch: Dispatch<RootState>, ownProps: SketchAreaOwnProps) {
+    return {
+        onLineAdded: (line: Line) => dispatch(addLine(ownProps.page.id, line))
+    };
+}
+
+export default connect<SketchAreaProps, SketchAreaDispatchProps, SketchAreaOwnProps, RootState>(
+    mapStateToProps,
+    mapDispatchToProps
+)(SketchArea);
