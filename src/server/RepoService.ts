c4badbe3847b1f82f6a7ce62a837647e2464e869
Thomas <Thomas@DevBook>
2017-12-28 19:43:47 +0100

    added commit, status and push git operations

--- src/server/RepoService.ts
+++ src/server/RepoService.ts
@@ -12,4 +12,40 @@ export class RepoService {
 
         return git.Clone.clone(url, localPath, { fetchOpts });
     }
+
+    public async addAllAndCommit(
+        repoPath: string,
+        commitMessage: string,
+        author: string,
+        authorEmail: string
+    ) {
+
+        const repo = await git.Repository.open(repoPath);
+        const index = await repo.refreshIndex();
+        await index.addAll(undefined!, undefined!);
+        index.write();
+        const oid = await index.writeTree();
+        const head = await git.Reference.nameToId(repo, "HEAD");
+        const parentCommit = await repo.getCommit(head);
+
+        const signature = git.Signature.now(author, authorEmail);
+        return repo.createCommit("HEAD", signature, signature, commitMessage, oid, [parentCommit]);
+    }
+
+    public async push(repoPath: string, username: string, password: string) {
+        const repo = await git.Repository.open(repoPath);
+        const remote = await repo.getRemote("origin");
+        return remote.push(["refs/heads/master:refs/heads/master"], {
+            callbacks: {
+                credentials: () => {
+                    return git.Cred.userpassPlaintextNew(username, password);
+                }
+            }
+        });
+    }
+
+    public async status(repoPath: string) {
+        const repo = await git.Repository.open(repoPath);
+        return repo.getStatus();
+    }
 }
