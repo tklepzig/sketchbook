c63b889624c56326080e656fe5171770ba5a60e8
Thomas <Thomas@DevBook>
2017-12-28 14:45:59 +0100

    use cmd args instead of config file

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,5 @@
 import * as path from "path";
 
-export const dataPath = "../../../data";
-export const configFile = path.resolve(__dirname, dataPath, "config.json");
+export const dataPath = path.resolve(__dirname, "../../../data");
 export const pageListFile = path.resolve(__dirname, dataPath, "page-list");
 export const pageDirectory = path.resolve(__dirname, dataPath, "pages");
