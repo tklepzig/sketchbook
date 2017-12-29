6f3e7c10aeaae5f4728ca88d4c20ed8d37b78b6d
Thomas <Thomas@DevBook>
2017-12-29 21:24:44 +0100

    corrected dataPath

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
 
-export const dataPath = path.resolve(__dirname, "../../../data");
+export const dataPath = path.resolve(__dirname, "data");
 export const pageListFile = path.resolve(__dirname, dataPath, "page-list");
 export const pageDirectory = path.resolve(__dirname, dataPath, "pages");
