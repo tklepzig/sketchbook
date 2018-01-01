de537b8a891c4eb4b249c9bc467afa1cdffa34b1
Thomas <Thomas@DevBook>
2018-01-01 12:40:32 +0100

    exception handling

--- src/server/RepoService.ts
+++ src/server/RepoService.ts
@@ -10,7 +10,13 @@ export class RepoService {
             }
         };
 
-        return git.Clone.clone(url, localPath, { fetchOpts });
+        try {
+            return git.Clone.clone(url, localPath, { fetchOpts });
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.log(`Error during git clone: ${error}`);
+            throw error;
+        }
     }
 
     public async addAllAndCommit(
@@ -19,33 +25,50 @@ export class RepoService {
         author: string,
         authorEmail: string
     ) {
+        try {
+            const repo = await git.Repository.open(repoPath);
+            const index = await repo.refreshIndex();
+            await index.addAll(undefined!, undefined!);
+            index.write();
+            const oid = await index.writeTree();
+            const head = await git.Reference.nameToId(repo, "HEAD");
+            const parentCommit = await repo.getCommit(head);
 
-        const repo = await git.Repository.open(repoPath);
-        const index = await repo.refreshIndex();
-        await index.addAll(undefined!, undefined!);
-        index.write();
-        const oid = await index.writeTree();
-        const head = await git.Reference.nameToId(repo, "HEAD");
-        const parentCommit = await repo.getCommit(head);
-
-        const signature = git.Signature.now(author, authorEmail);
-        return repo.createCommit("HEAD", signature, signature, commitMessage, oid, [parentCommit]);
+            const signature = git.Signature.now(author, authorEmail);
+            return repo.createCommit("HEAD", signature, signature, commitMessage, oid, [parentCommit]);
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.log(`Error during git add and commit: ${error}`);
+            throw error;
+        }
     }
 
     public async push(repoPath: string, username: string, password: string) {
-        const repo = await git.Repository.open(repoPath);
-        const remote = await repo.getRemote("origin");
-        return remote.push(["refs/heads/master:refs/heads/master"], {
-            callbacks: {
-                credentials: () => {
-                    return git.Cred.userpassPlaintextNew(username, password);
+        try {
+            const repo = await git.Repository.open(repoPath);
+            const remote = await repo.getRemote("origin");
+            return remote.push(["refs/heads/master:refs/heads/master"], {
+                callbacks: {
+                    credentials: () => {
+                        return git.Cred.userpassPlaintextNew(username, password);
+                    }
                 }
-            }
-        });
+            });
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.log(`Error during git push: ${error}`);
+            throw error;
+        }
     }
 
     public async status(repoPath: string) {
-        const repo = await git.Repository.open(repoPath);
-        return repo.getStatus();
+        try {
+            const repo = await git.Repository.open(repoPath);
+            return repo.getStatus();
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.log(`Error during retrieving git status: ${error}`);
+            throw error;
+        }
     }
 }
