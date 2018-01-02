648922024f253e9647cce70d4f04c5eaf5968d4a
Thomas <Thomas@DevBook>
2018-01-02 13:02:44 +0100

    auth test 2

--- src/public/actions.ts
+++ src/public/actions.ts
@@ -153,9 +153,7 @@ export const fetchPageList = () => async (dispatch: Dispatch<RootState>) => {
             mode: "cors",
             headers: {
                 "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Credentials": "true",
-                "Access-Control-Allow-Origin": "*"
+                "Content-Type": "application/json"
             }
         });
 
