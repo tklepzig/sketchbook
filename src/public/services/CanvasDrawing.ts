b0494d8f9bd19979f84edd69da2d955e15b6844b
Thomas <Thomas@DevBook>
2018-01-13 22:43:22 +0100

    removed disabled code for grouping lines by width and color

--- src/public/services/CanvasDrawing.ts
+++ src/public/services/CanvasDrawing.ts
@@ -71,19 +71,11 @@ export class CanvasDrawing {
             right += 40;
             bottom += 40;
 
-            // if (props.lines.length > 4) {
-            // faster, bot wrong detail (lines are not in the right order)
-            // drawLinesSortedAndGrouped(context, { left, top, right, bottom });
-            // } else {
             if (limitToVisibleArea) {
-                this.drawPageElements(
-                    context,
-                    elements,
-                    { left, top, right, bottom });
+                this.drawPageElements(context, elements, { left, top, right, bottom });
             } else {
                 this.drawPageElements(context, elements);
             }
-            // }
 
             context.lineWidth = savedLineWidth;
             context.strokeStyle = savedStrokeStyle;
@@ -190,75 +182,4 @@ export class CanvasDrawing {
         }
         return { width, height };
     }
-
-    // private refreshLinesGroupedByColorAndWidth() {
-    //     this.linesGroupedByColorAndWidth =
-    //         groupBy(
-    //             this.props.lines.sort((l1: Line, l2: Line) => l1.color > l2.color ? 1 : -1),
-    //             (line) => `${line.color}|${line.lineWidth}|${line.globalCompositeOperation}`);
-    // }
-
-    // private drawLinesSortedAndGrouped(
-    //     canvasContext: CanvasRenderingContext2D,
-    //     boundary: {
-    //         left: number,
-    //         top: number,
-    //         right: number,
-    //         bottom: number
-    //     }) {
-
-    //     const { top, left, right, bottom } = boundary;
-
-    //     for (const key in this.linesGroupedByColorAndWidth) {
-    //         if (!this.linesGroupedByColorAndWidth.hasOwnProperty(key)) {
-    //             continue;
-    //         }
-
-    //         const lines = this.linesGroupedByColorAndWidth[key];
-
-    //         canvasContext.beginPath();
-    //         const [color, lineWidth, globalCompositeOperation] = key.split("|");
-    //         canvasContext.strokeStyle = color;
-    //         canvasContext.lineWidth = +lineWidth;
-    //         canvasContext.globalCompositeOperation = globalCompositeOperation;
-
-    //         lines.forEach((line: Line) => {
-    //             line.segments.forEach((segment) => {
-    //                 if (
-    //                     (segment.start.x > left
-    //                         && segment.start.x < right
-    //                         && segment.start.y > top
-    //                         && segment.start.y < bottom)
-    //                     ||
-    //                     (segment.end.x > left
-    //                         && segment.end.x < right
-    //                         && segment.end.y > top
-    //                         && segment.end.y < bottom)) {
-    //                     canvasContext.moveTo(segment.start.x, segment.start.y);
-    //                     canvasContext.lineTo(segment.end.x, segment.end.y);
-    //                 }
-    //             });
-    //         });
-
-    //         canvasContext.stroke();
-    //         canvasContext.closePath();
-    //     }
-    // }
 }
-
-// from https://gist.github.com/bryandh/c0cd25df7ccfdf891bf782174535f4ca
-// function groupBy<T>(collection: T[], property: (item: T) => any): any {
-//     return collection.reduce(
-//         (prev: any, next: any) => {
-//             // Get the group name from the property function, this will be the index of the group in the array
-//             const groupName = property(next);
-//             // Set the group in the array to either its previous value (prev[groupName])
-//             // if that exists, otherwise fill it with an empty array
-//             prev[groupName] = prev[groupName] || [];
-//             // Add the next item to the group's array
-//             prev[groupName].push(next);
-//             return prev;
-//         },
-//         Object.create([])
-//     );
-// }
