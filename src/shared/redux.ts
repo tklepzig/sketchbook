1147d16e77c72e232ffffb9e9bff3651a558744f
Thomas <Thomas@DevBook>
2018-01-13 11:29:41 +0100

    type-safe combineReducers

--- src/shared/redux.ts
+++ src/shared/redux.ts
@@ -2,10 +2,17 @@ import * as redux from "redux";
 
 export type Reducer<S, A> = (state: S, action: A) => S;
 
-export interface ReducersMapObject {
-  [key: string]: Reducer<any, any>;
-}
+export type ReducersMapObject<S> = {
+  [P in keyof S]: Reducer<S[P], any>;
+};
 
-export function combineReducers<S>(reducers: ReducersMapObject): Reducer<S, any> {
-  return redux.combineReducers(reducers);
+export function combineReducers<S>(reducers: ReducersMapObject<S>): Reducer<S, any> {
+  const map: redux.ReducersMapObject = {};
+  for (const key in reducers) {
+    if (reducers.hasOwnProperty(key)) {
+      const reducer = reducers[key];
+      map[key] = reducer;
+    }
+  }
+  return redux.combineReducers(map);
 }
