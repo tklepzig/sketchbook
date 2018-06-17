609ffc9423e16b2bce1ca9b6a5fdd3ff7fd348c3
Thomas <Thomas@DevBook>
2018-06-17 13:45:01 +0200

    added first draft zoom & translate to overview (#12), needs refactoring

--- src/public/components/Page/Overview/OverviewCanvas.tsx
+++ src/public/components/Page/Overview/OverviewCanvas.tsx
@@ -1,11 +1,13 @@
 import { CanvasContext } from "@services/CanvasContext";
 import { CanvasDrawing } from "@services/CanvasDrawing";
 import canvasHelper from "@services/CanvasHelper";
+import { CanvasTranslate } from "@services/CanvasTranslate";
 import pageElementHelper from "@services/PageElementHelper";
 import { tapEvents } from "@services/TapEvents";
 import { Line, PageElement, Point } from "@shared/models";
 import * as React from "react";
 import { NavLink } from "react-router-dom";
+import { bind } from "react.ex";
 
 interface OverviewCanvasProps {
     elements: PageElement[];
@@ -22,6 +24,10 @@ export class OverviewCanvas extends React.Component<OverviewCanvasProps, Overvie
 
     private canvasContext: CanvasContext;
     private canvasDrawing: CanvasDrawing;
+    private canvasTranslate: CanvasTranslate;
+    private tapIsDown = false;
+    private isTranslateZoomMode = false;
+    private lastDistance = 0;
 
     constructor(props: OverviewCanvasProps) {
         super(props);
@@ -31,15 +37,18 @@ export class OverviewCanvas extends React.Component<OverviewCanvasProps, Overvie
         this.state = { scale: 1, translation: { dx: 0, dy: 0 } };
         this.canvasContext = new CanvasContext(() => this.canvas == null ? null : this.canvas.getContext("2d"));
         this.canvasDrawing = new CanvasDrawing();
+        this.canvasTranslate = new CanvasTranslate();
     }
 
     public componentDidMount() {
         this.resize();
         window.addEventListener("resize", this.resize);
+        window.addEventListener("mousewheel", this.onMouseWheel);
     }
 
     public componentWillUnmount() {
         window.removeEventListener("resize", this.resize);
+        window.removeEventListener("mousewheel", this.onMouseWheel);
     }
 
     public componentWillReceiveProps() {
@@ -53,10 +62,113 @@ export class OverviewCanvas extends React.Component<OverviewCanvasProps, Overvie
                 style={{ cursor: "default" }}
                 ref={(canvas) => { this.canvas = canvas; }}
                 {...{ [tapEvents.tapUp]: this.tapUp }}
+                {...{ [tapEvents.tapDown]: this.tapDown }}
+                {...{ [tapEvents.tapMove]: this.tapMove }}
             />);
     }
 
+    @bind
+    private onMouseWheel(e: any) {
+        if (!e.ctrlKey) {
+            return true;
+        }
+
+        const delta = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
+
+        if (delta) {
+            const scaleFactor = 1.1;
+            const factor = Math.pow(scaleFactor, delta);
+
+            const downPoint = tapEvents.getTapPosition(e);
+            // add offset since canvas is not at position 0, 0
+            const { x, y } = this.canvasContext.getPosition();
+            downPoint.x -= x;
+            downPoint.y -= y;
+            const pt = this.canvasContext.getTransformedPoint(downPoint);
+            this.canvasContext.translate(pt.x, pt.y);
+            this.canvasContext.scale(factor, factor);
+            this.canvasContext.translate(-pt.x, -pt.y);
+            this.canvasDrawing.repaint(this.canvasContext, this.props.elements, false);
+        }
+
+        e.preventDefault();
+        return false;
+    }
+
+    @bind
+    private tapDown(e: any) {
+        this.tapIsDown = true;
+        const touchCount = tapEvents.getTouchCount(e);
+        this.isTranslateZoomMode = touchCount === 2 || e.ctrlKey;
+        const originalTapDownPoint = tapEvents.getTapPosition(e);
+        const tapDownPoint = this.canvasContext.getTransformedPoint(originalTapDownPoint);
+
+        if (this.isTranslateZoomMode) {
+            this.canvasTranslate.startTranslate(tapDownPoint);
+        }
+    }
+
+    @bind
+    private tapMove(e: any) {
+        if (!this.tapIsDown) {
+            return;
+        }
+        if (this.isTranslateZoomMode) {
+
+            const translate = (point: Point) => {
+                // add offset since canvas is not at position 0, 0
+                const { x, y } = this.canvasContext.getPosition();
+                point.x -= x;
+                point.y -= y;
+                const pt = this.canvasContext.getTransformedPoint(point);
+                this.canvasTranslate.translate(this.canvasContext, pt);
+                this.canvasDrawing.repaint(this.canvasContext, this.props.elements, false);
+            };
+
+            const center = tapEvents.getPinchZoomCenter(e);
+            if (!center) {
+                return;
+            }
+
+            const distance = tapEvents.getPinchZoomDistance(e);
+            if (distance) {
+
+                if (this.lastDistance === 0) {
+                    this.lastDistance = distance;
+                } else if (Math.abs(this.lastDistance - distance) > 20) {
+
+                    const scaleFactor = 1.1;
+                    const factor = Math.pow(scaleFactor, -(this.lastDistance - distance) / 20);
+
+                    // add offset since canvas is not at position 0, 0
+                    const { x, y } = this.canvasContext.getPosition();
+                    center.x -= x;
+                    center.y -= y;
+                    const pt = this.canvasContext.getTransformedPoint(center);
+                    this.canvasContext.translate(pt.x, pt.y);
+                    this.canvasContext.scale(factor, factor);
+                    this.canvasContext.translate(-pt.x, -pt.y);
+                    this.canvasDrawing.repaint(this.canvasContext, this.props.elements, false);
+                    this.lastDistance = distance;
+                } else {
+                    translate(center);
+                }
+            } else {
+                translate(center);
+            }
+        }
+    }
+
     private tapUp(e: any) {
+        if (!this.tapIsDown) {
+            return;
+        }
+        this.tapIsDown = false;
+        this.lastDistance = 0;
+
+        if (this.isTranslateZoomMode) {
+            return;
+        }
         const downPoint = tapEvents.getTapPosition(e);
         // add offset since canvas is not at position 0, 0
         const { x, y } = this.canvasContext.getPosition();
